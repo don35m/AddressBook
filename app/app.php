8d92dbb400f3449a385011384e66a9a68e061900
Don <dons500@gmail.com>
2015-08-07 14:25:19 -0700

    Add files and mostly working

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/contact.php";
+    require_once __DIR__."/../src/Contact.php";
 
     session_start();
 
@@ -14,59 +14,24 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() {
+    $app->get("/", function() use ($app) {
 
-        $output = "";
 
-        $all_contacts = Contact::getAll();
-
-        if (!empty($all_contacts)) {
-            $output .= "
-                <h1>Contact List</h1>
-                <p>Here are all your contacts:</p>
-            ";
-
-            foreach (Contact::getAll() as $contact) {
-                $output .= "<p>" . $contact->getDescription() . "</p>";
-            }
-        }
-
-        $output .= "
-            <form action='/contacts' method='post'>
-                <label for='description'>Contact Description</label>
-                <input id='description' name='description' type='text'>
-
-                <button type='submit'>Add Contact</button>
-            </form>
-        ";
-
-        $output .= "
-            <form action='/delete_contacts' method='post'>
-                <button type='submit'>Clear</button>
-            </form>
-        ";
-
-        return $output;
+        return $app['twig']->render('contact.html.twig' ,array('contacts' => Contact::getAll()));
     });
 
-    $app->post("/contacts", function() {
-        $contact = new Contact($_POST['description']);
+    $app->post("/contact", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
         $contact->save();
-        return "
-            <h1>You created a contact!</h1>
-            <p>" . $contact->getDescription() . "</p>
-            <p><a href='/'>View your list of contacts.</a></p>
-        ";
+        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+
     });
 
-    $app->post("/delete_contacts", function() {
+    $app->post("/delete_contact", function() use ($app) {
 
         Contact::deleteAll();
+        return $app['twig']->render('delete_contact.html.twig');
 
-        return "
-            <h1>List cleared!</h1>
-            <p><a href='/'>Home</a></p>
-        ";
     });
 
 
